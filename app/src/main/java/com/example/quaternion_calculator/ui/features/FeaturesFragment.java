858a088dd7ee84f39e7ed9838a9be2e540fbbdc3
lenaaa-l <lena.libon@yahoo.de>
2020-08-19 10:24:59 +0200

    Add feature functionality

--- app/src/main/java/com/example/quaternion_calculator/ui/features/FeaturesFragment.java
+++ app/src/main/java/com/example/quaternion_calculator/ui/features/FeaturesFragment.java
@@ -13,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.quaternion_calculator.R;
+import com.example.quaternion_calculator.quaternions.Quaternion;
 
 public class FeaturesFragment extends Fragment {
 
@@ -31,11 +32,76 @@ public class FeaturesFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 // Read quaternion
-                // TODO
+                // Check for emptiness -> if this is the case enter 0.0 in the box
+                // Save the values
+                double s_value, x_value, y_value, z_value;
+
+                if (s.getText().toString().isEmpty()) {
+                    s.setText("0.0");
+                    s_value = 0;
+                } else
+                    s_value = Double.parseDouble(s.getText().toString());
+
+                if (x.getText().toString().isEmpty()) {
+                    x.setText("0.0");
+                    x_value = 0;
+                } else
+                    x_value = Double.parseDouble(x.getText().toString());
+
+                if (y.getText().toString().isEmpty()) {
+                    y.setText("0.0");
+                    y_value = 0;
+                } else
+                    y_value = Double.parseDouble(y.getText().toString());
+
+                if (z.getText().toString().isEmpty()) {
+                    z.setText("0.0");
+                    z_value = 0;
+                } else
+                    z_value = Double.parseDouble(z.getText().toString());
+
+                // Create Quaternions
+                Quaternion q = new Quaternion(s_value, x_value, y_value, z_value);
+
+                // Set brackets to visible
+                open_bracket.setVisibility(View.VISIBLE);
+                closed_bracket.setVisibility(View.VISIBLE);
+
+                // Real part
+                real_part.setText(s_value + "");
+
+                // Imaginary part
+                x_imaginary_part.setText(x_value + "");
+                y_imaginary_part.setText(y_value + "");
+                z_imaginary_part.setText(z_value + "");
+
+                // Conjuate
+                conjugate.setText(q.conjugate().toString());
+
+                // Norm
+                norm.setText(roundHelper(q.norm(), 5) + "");
+
+                // Reciprocal
+                Quaternion r = q.reciprocal();
+                r.setS(roundHelper(r.getS(), 2));
+                r.setX(roundHelper(r.getX(), 2));
+                r.setY(roundHelper(r.getY(), 2));
+                r.setZ(roundHelper(r.getZ(), 2));
+                reciprocal.setText(r.toString());
             }
         });
 
         return root;
+    }
+
+    /**
+     * @param value Value to be rounded
+     * @param decimalPlaces number of decimal places
+     * @return rounded value
+     */
+    private double roundHelper(double value, int decimalPlaces){
+        double t = Math.pow(10, decimalPlaces);
+        return Math.round(value*t) / t;
     }
 
     private void init(View root) {
@@ -59,7 +125,7 @@ public class FeaturesFragment extends Fragment {
         norm = root.findViewById(R.id.norm_features);
         reciprocal = root.findViewById(R.id.reciprocal_features);
 
-        // Set brackets unvisible
+        // Univisible brackets
         open_bracket.setVisibility(View.GONE);
         closed_bracket.setVisibility(View.GONE);
     }
