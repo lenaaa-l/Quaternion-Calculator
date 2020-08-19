dde1262a182705abebaa12d488db68539092b9e5
lenaaa-l <lena.libon@yahoo.de>
2020-08-19 02:03:59 +0200

    Add feature page

---
+++ app/src/main/java/com/example/quaternion_calculator/ui/features/FeaturesFragment.java
@@ -0,0 +1,22 @@
+package com.example.quaternion_calculator.ui.features;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.quaternion_calculator.R;
+
+public class FeaturesFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_features, container, false);
+
+        return root;
+    }
+}
